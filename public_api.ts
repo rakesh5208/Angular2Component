48eee4b4f881601fccd94e7fff200556fa6fd7c1
Mani <rakesh5208@gmail.com>
2018-06-11 19:27:29 +0530

    added all exports in public_api.ts

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,14 @@
-export * from './src/app/core/loading/loading.module';
+// loading module
+export { LoadingModule } from './src/app/core/loading/loading.module';
+// page top header navigation module
+export { NavItem } from './src/app/core/nav/nav-item';
+export { NavModule } from './src/app/core/nav/nav.module';
+// page footer module
+export { FooterModule } from './src/app/core/footer/footer.module';
+//page body module
+export { PageBodyModule } from './src/app/core/page-body/page-body.module';
+// Notification Module
+export { NotifyConfig, NotificationType, NotificationService, NotificationState } from './src/app/core/notification/notification.service';
+export { NotificationModule } from './src/app/core/notification/notification.module';
+// Ui pop up or modal module
+export { ModalModule } from './src/app/core/modal/modal.module';
